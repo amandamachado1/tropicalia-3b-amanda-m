074e41bf475636a2e3fadcf8b0eb56fb7891b538
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-04-01 08:25:19 -0300

    aaaaaaa

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
     let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click',function()){
+    aumentaFonteBotao.addEventListener('click', function(){
      tamanhoAtualFonte += 0.1;
      document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
