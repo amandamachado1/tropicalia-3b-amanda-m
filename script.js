5fcf602fade28a097d7e6fe608bff4ef0f162304
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-04-22 08:44:48 -0300

    aaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     })
+   const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded')==='true'; 
+   botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     
